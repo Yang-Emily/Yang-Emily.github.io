592d0e629a3d67ecee0e3f69f34fb04124a07599
Yang-Emily <yang.wu@inf.ethz.ch>
2026-07-20 15:25:28 +0200

    update

--- src/components/News.jsx
+++ src/components/News.jsx
@@ -13,7 +13,7 @@ const News = () => {
     {
       date: 'Jan 2026',
       title: 'Paper accepted at IUI 2026',
-      description: 'Our paper "StepMIND" has been accepted to IUI 2026 and received the Best Paper Award! Looking forward to seeing you in Cyprus!',
+      description: 'Our paper "StepMIND" was presented at IUI 2026 in Cyprus and received the Best Paper Award!',
       icon: <FileText size={20} />,
       type: 'publication',
       color: 'yellow',
